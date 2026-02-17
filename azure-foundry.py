8ff8f8bf59dfa7abc727f8e72ce8532d3d889e02
Ashwani Garg <ashwanigarg@outlook.in>
2026-02-17 14:27:31 +0530

    merge

--- azure-foundry.py
+++ azure-foundry.py
@@ -1,12 +1,13 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from azure.identity import DefaultAzureCredential
 from azure.ai.projects import AIProjectClient
 from azure.ai.projects.models import PromptAgentDefinition
 from database import Base, Thread, SessionLocal, engine, Agent
-
+from pprint import pprint
 
 load_dotenv()
+agent_name = os.environ["AGENT_NAME"]
 
 project_client = AIProjectClient(
     endpoint=os.environ["PROJECT_ENDPOINT"],
@@ -23,15 +24,9 @@ def get_create_agent():
         return db_agents[0].agent_id
     
     agents = list(project_client.agents.list())
-    
+
     if len(agents) == 0:
         # Create persistent agent
-        # agent = project_client.agents.create_agent(
-        #     model="gpt-4o-mini",
-        #     name=os.environ["AGENT_NAME"],
-        #     instructions="You are a senior backend developer helping with Python and Azure architecture.",
-        # )
-
         agent = project_client.agents.create_version(
             agent_name=os.environ["AGENT_NAME"],
             definition=PromptAgentDefinition(
@@ -39,11 +34,11 @@ def get_create_agent():
                 instructions="You are a helpful assistant that answers general questions",
             ),
         )
-
+   
         db.add(Agent(
             agent_id = agent.id,
             agent_name = agent.name,
-            agent_model = agent.model 
+            agent_model = agent.versions.latest.definition.model
         ))
         db.commit()
         return agent.id
@@ -53,25 +48,25 @@ def get_create_agent():
             db.add(Agent(
                 agent_id = agent.id,
                 agent_name = agent.name,
-                agent_model = agent.model 
+                agent_model = agent.versions.latest.definition.model
             ))    
         db.commit()
         return agent_id
 
 agent_id = get_create_agent()
+openai_client = project_client.get_openai_client()
 
 def get_create_thread():
     db = SessionLocal()
     db_threads = db.query(Thread).all()
-
+    
     if len(db_threads) == 0:
-        # Create persistent thread
-        thread = project_client.agents.threads.create()
+        conversation = openai_client.conversations.create()
         db.add(Thread(
-            thread_id = thread.id,
+            thread_id = conversation.id,
         ))
         db.commit()
-        return thread.id
+        return conversation.id
     else:
         for thread in db_threads:
             thread_id = thread.thread_id
@@ -79,42 +74,12 @@ def get_create_thread():
 
 thread_id = get_create_thread()
 
-
 print(f"Created agent, ID: {agent_id}")
 
-# create an agent thread
-# thread = project_client.agents.threads.create()
-# print(f"thread id: {thread.id}")
-
-# add message to a thread
-message = project_client.agents.messages.create(
-    thread_id=thread_id,
-    role="user",
-    content="What are the benefits of using an Agent over a simple LLM call?"
+response = openai_client.responses.create(
+    conversation=thread_id, #Optional conversation context for multi-turn
+    extra_body={"agent": {"name": agent_name, "type": "agent_reference"}},
+    input="What is the size of France in square miles?",
 )
 
-# 5. Run the Agent on the Thread
-# 'create_and_process' is the standard method for the Foundry SDK to wait for completion
-run = project_client.agents.runs.create_and_process(
-    thread_id=thread_id, 
-    agent_id=agent_id
-)
-
-# print(f"Run Status: {run.status}")
-
-# 6. Retrieve and print the response
-if run.status == "completed":
-    # Get the messages (this returns an iterator)
-    messages = project_client.agents.messages.list(thread_id=thread_id)
-    
-    # Convert the iterator to a list to access by index
-    messages_list = list(messages)
-    
-    # The newest message is at index 0
-    last_msg = messages_list[0]
-    
-    if last_msg.role == "assistant":
-        # Access the text content properly
-        # Content is a list of MessageContent objects
-        print(f"\nAgent Response: {last_msg.content[0].text.value}")
-
+print(response.output_text)
